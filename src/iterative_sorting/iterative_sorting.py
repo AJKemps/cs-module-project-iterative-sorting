3245e5f5f89e093490b72fd4296f486a9bd816b3
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-14 17:55:58 -0400

    bubble sort is complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -25,6 +25,17 @@ def selection_sort(arr):
 def bubble_sort(arr):
     # Your code here
 
+    indexing_length = len(arr) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+
+        for i in range(0, indexing_length):
+            if arr[i] > arr[i+1]:
+                sorted = False
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+
     return arr
 
 
